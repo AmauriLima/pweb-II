4d63cc0fc8ba54a12718387176819cd51596817e
AmauriLima <amauri.plimaj@gmail.com>
2025-02-19 13:22:06 -0300

    style: changes docs architecture

--- src/application/modules/accounts/docs/accounts-path.ts
+++ src/application/modules/accounts/docs/accounts-path.ts
@@ -1,8 +1,8 @@
 import { PathItem } from "swagger-jsdoc";
-import { createAccountSwagger } from "../use-cases/create-account/swagger";
-import { deleteAccountSwagger } from "../use-cases/delete-account/swagger";
-import { getAccountsSwagger } from "../use-cases/get-accounts/swagger";
-import { updateAccountSwagger } from "../use-cases/update-account/swagger";
+import { createAccountSwagger } from "./create-account-swagger";
+import { deleteAccountSwagger } from "./delete-account-swagger";
+import { getAccountsSwagger } from "./get-accounts-swagger";
+import { updateAccountSwagger } from "./update-account-swagger";
 
 export const accountsPath: PathItem = {
   post: createAccountSwagger,
