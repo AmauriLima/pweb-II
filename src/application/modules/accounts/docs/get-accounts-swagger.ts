dbf28a8a106c609097977d43e99066bd1097289b
AmauriLima <amauri.plimaj@gmail.com>
2025-02-21 19:34:12 -0300

    docs: adds get accounts error treatment

--- src/application/modules/accounts/docs/get-accounts-swagger.ts
+++ src/application/modules/accounts/docs/get-accounts-swagger.ts
@@ -1,6 +1,8 @@
 import { Tags } from "@/@types/tags";
 import { Operation } from "swagger-jsdoc";
 
+export const GET_ACCOUNTS_ERROR = 'Erro ao listar usuários';
+
 export const getAccountsSwagger: Operation = {
   tags: [Tags.ACCOUNTS],
   summary: 'Lista todas as contas',
@@ -17,5 +19,18 @@ export const getAccountsSwagger: Operation = {
         },
       },
     },
+    '500': {
+      description: 'Erro interno do servidor',
+      content: {
+        'application/json': {
+          schema: { $ref: '#/components/schemas/ErrorsResponse' },
+          examples: {
+            'Erro ao listar': {
+              value: { messages: [GET_ACCOUNTS_ERROR] }
+            }
+          }
+        },
+      },
+    }
   },
 }
