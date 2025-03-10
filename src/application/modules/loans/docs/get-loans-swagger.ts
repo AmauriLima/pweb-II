7a3fc0e868be51d2f3411f93888f6f2fc3c1d3c0
AmauriLima <amauri.plimaj@gmail.com>
2025-03-10 11:38:22 -0300

    feat: adds get loans by account id option

--- src/application/modules/loans/docs/get-loans-swagger.ts
+++ src/application/modules/loans/docs/get-loans-swagger.ts
@@ -5,6 +5,17 @@ export const getLoansSwagger: Operation = {
   tags: [Tags.LOANS],
   summary: 'Lista todos os empréstimos',
   description: 'Retorna uma lista com todos os empréstimos cadastrados',
+  parameters: [
+    {
+      name: "accountId",
+      in: "query",
+      description: "Uuid da conta que os expréstimos estão vinculados ",
+      required: false,
+      schema: {
+        type: "string",
+      },
+    },
+  ],
   responses: {
     '200': {
       description: 'Lista de empréstimos retornada com sucesso',
