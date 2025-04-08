4e28c46af6d8724a7cbc93884404b38fd503516a
AmauriLima <amauri.plimaj@gmail.com>
2025-04-07 21:56:01 -0300

    hotfix

--- src/application/modules/loans/docs/get-my-loans-swagger.ts
+++ src/application/modules/loans/docs/get-my-loans-swagger.ts
@@ -5,14 +5,46 @@ export const getMyLoansSwagger: Operation = {
   tags: [Tags.LOANS],
   summary: 'Lista todos os empréstimos associados a minha conta',
   description: 'Retorna uma lista com todos os empréstimos associados a minha conta',
+  parameters: [
+    {
+      name: "page",
+      in: "query",
+      description: "Página atual para paginação",
+      required: false,
+      schema: {
+        type: "integer",
+        default: 1,
+      },
+    },
+    {
+      name: "perPage",
+      in: "query",
+      description: "Quantidade de itens por página",
+      required: false,
+      schema: {
+        type: "integer",
+        default: 10,
+      },
+    },
+  ],
   responses: {
     '200': {
       description: 'Lista de empréstimos retornada com sucesso',
       content: {
         'application/json': {
           schema: {
-            type: 'array',
-            items: { $ref: '#/components/schemas/LoanResponse' },
+            type: 'object',
+            properties: {
+              data: {
+                type: 'array',
+                items: { $ref: '#/components/schemas/LoanResponse' },
+              },
+              nextCursor: {
+                type: 'string',
+                nullable: true,
+                description: "Cursor para a próxima página. Null indica que não há mais resultados.",
+              },
+            },
           },
         },
       },
--- src/application/modules/loans/use-cases/get-loans/get-my-loans-controller.ts
+++ src/application/modules/loans/use-cases/get-loans/get-my-loans-controller.ts
@@ -2,6 +2,7 @@ import { IController } from "@/application/shared/http/interfaces/controller";
 import { IHttpRequest, IHttpResponse } from "@/application/shared/http/interfaces/http";
 import { HttpResponse } from "@/application/shared/http/response/http-response";
 import { LoanMapper } from "../../mappers/loan-mapper";
+import { getLoansSchema } from "./get-loans-dto";
 import { GetLoansUseCase } from "./get-loans-use-case";
 
 export class GetMyLoansController implements IController {
@@ -10,11 +11,19 @@ export class GetMyLoansController implements IController {
   ) {}
 
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    const { loans } = await this.useCase.execute({ accountId: request.account?.id });
+    const {
+      page,
+      perPage,
+    } = getLoansSchema.parse(request.query);
+
+    const { loans, totalLoans } = await this.useCase.execute({ accountId: request.account?.id, page, perPage });
 
     return HttpResponse.ok({
-      body: loans.map(LoanMapper.toHttp)
+      body: {
+        totalItems: totalLoans,
+        data: loans.map(LoanMapper.toHttp),
+      },
     });
   }
-  
+
 }
